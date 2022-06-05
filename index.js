fcc732bb08867398166281ab8fb0381eba5e4419
lukassemmler <kontakt@lukassemmler.de>
2022-06-05 14:01:56 +0200

    Add second example palette

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const diff = require('color-diff');
 const util = require('./lib/util');
 const ral = require('./data/ral-palette.json');
-const example = require('./data/example-palette.json');
+const example = require('./data/example-palette-2.json');
 
 const rgbByName = new Map();
 for (let color of ral.palette)
@@ -12,5 +12,6 @@ for (let color of example.palette) {
   const rgb = util.hexToRgb(color.hex);
   const closest = diff.closest(rgb, rawColors);
   const closestName = util.getKeyByValue(rgbByName, closest);
-  console.log(color.hex + " --> " + closestName + " " + ral.palette.find(color => color.name === closestName).hex);
+  const ralColor = ral.palette.find(color => color.name === closestName);
+  console.log(color.hex + " --> " + ralColor.hex + " - " + ralColor.code + " " + closestName);
 }

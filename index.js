a68448c88d40b0171a2203e11c766903a686581d
lukassemmler <kontakt@lukassemmler.de>
2022-06-05 14:37:48 +0200

    Fix color palette comparison

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const diff = require('color-diff');
 const util = require('./lib/util');
-const ral = require('./data/ral-palette.json');
+const ral = require('./data/ral-classic.json');
 const example = require('./data/example-palette-2.json');
 
 const rgbByName = new Map();
-for (let color of ral.palette)
-  rgbByName.set(color.name, util.hexToRgb(color.hex));
+for (let color of ral.colors)
+  rgbByName.set(color.nameGerman, util.hexToRgb(color.hex));
 const rawColors = Array.from(rgbByName.values());
 
-for (let color of example.palette) {
+for (let color of example.colors) {
   const rgb = util.hexToRgb(color.hex);
   const closest = diff.closest(rgb, rawColors);
   const closestName = util.getKeyByValue(rgbByName, closest);
-  const ralColor = ral.palette.find(color => color.name === closestName);
-  console.log(color.hex + " --> " + ralColor.hex + " - " + ralColor.code + " " + closestName);
+  const ralColor = ral.colors.find(color => color.nameGerman === closestName);
+  console.log(`${color.hex} --> ${ralColor.hex} - ${ralColor.palette} ${ralColor.number} - ${closestName}`);
 }

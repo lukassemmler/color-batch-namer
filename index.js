8949b6173199e4c5dc53ab998d0392df32adc0c6
lukassemmler <kontakt@lukassemmler.de>
2022-06-05 14:58:15 +0200

    Restructure project to use a bin folder

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-const diff = require('color-diff');
-const util = require('./lib/util');
-const ral = require('./data/ral-classic.json');
-const example = require('./data/example-palette-2.json');
-
-const rgbByName = new Map();
-for (let color of ral.colors)
-  rgbByName.set(color.nameGerman, util.hexToRgb(color.hex));
-const rawColors = Array.from(rgbByName.values());
-
-for (let color of example.colors) {
-  const rgb = util.hexToRgb(color.hex);
-  const closest = diff.closest(rgb, rawColors);
-  const closestName = util.getKeyByValue(rgbByName, closest);
-  const ralColor = ral.colors.find(color => color.nameGerman === closestName);
-  console.log(`${color.hex} --> ${ralColor.hex} - ${ralColor.palette} ${ralColor.number} - ${closestName}`);
-}
+const compareColors = require('./bin/compare-colors.js').compareColors;
+compareColors();
